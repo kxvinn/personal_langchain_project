1a5dee754db05db5c8925e7b07baf72ec2e5b410
Kevin Dourado <102863598+kxvinn@users.noreply.github.com>
2025-03-01 19:18:48 -0300

    Update langchained.py

--- langchained.py
+++ langchained.py
@@ -14,12 +14,24 @@ apikey = os.getenv("KEY")
 
 mixtral = "mixtral-8x7b-32768"
 
+#initalizing Groq client
 chat = ChatGroq(model_name=mixtral, groq_api_key=apikey)
+
+# system prompt
+SYSTEM_PROMPT = """
+Você é um assistente virtual inteligente e prestativo. Sua tarefa é fornecer respostas claras, concisas e úteis para as perguntas dos usuários. Siga estas diretrizes ao responder:
+
+1. Seja contextual: Entenda o contexto da pergunta e forneça respostas relevantes.
+2. Seja educado e amigável: Use um tom profissional, mas acessível.
+3. Seja breve: Evite respostas longas e desnecessárias, a menos que o usuário peça mais detalhes.
+4. Seja preciso: Se não souber a resposta, diga claramente que não sabe e ofereça ajuda em outro tópico.
+5. Seja proativo: Antecipe possíveis dúvidas relacionadas e sugira informações adicionais quando apropriado.
+"""
 
 
 def get_groq_response(user_input):
     messages = [
-        SystemMessage(content="Você é o GPT-4. Responda tudo seguindo à risca as normas e programações do GPT-4."),
+        SystemMessage(content=SYSTEM_PROMPT),
 
         HumanMessage(content=user_input)
     ]
